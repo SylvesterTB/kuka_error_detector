7bf1910960aeabcf2232e20b3493088498022f11
SylvesterB <sylvester.broich@gmail.com>
2023-07-17 16:23:14 +0200

    optimization and logging

--- error_dect.py
+++ error_dect.py
@@ -1,4 +1,8 @@
 import paramiko
+from telegram_2 import *
+import time
+import logging
+
 
 hostname = '172.27.5.126'
 port = 22
@@ -22,13 +26,33 @@ with paramiko.SSHClient() as client:
     print("error:"+str(error, 'utf8'))
 
     if error == '':
-        print('no error')
+        error_msg = ''
     else: 
-        print('There is an error, send message vie telegram')
+        error_msg = 'There is an error'
     if output == '':
-        print('There is no output')
+        out_message = 'There is no output'
     else: 
-        print('output detected')
+        out_message = 'output detected'
+        
 
+logging.basicConfig(filename="logging_info.log", 
+					format='%(asctime)s %(message)s', 
+					filemode='w') 
+logging.basicConfig(filename='logging_info.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logger=logging.getLogger() 
+logger.setLevel(logging.INFO) 
+
+
+total_time = 0
+
+while(total_time <= 10):
+    total_time += 1
+    if out_message == "output detected":
+        telebot('',out_message)
+        logging.info('output detected')
+    else:
+        telebot(error_msg, out_message)
+        logging.info('There was no output detected, an error was also detected')
+    time.sleep(60)
 
 # ssh kuka@172.27.5.126
