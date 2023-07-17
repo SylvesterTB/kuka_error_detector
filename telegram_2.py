9db389b6cd96381f441fe0f8a477f65879269899
SylvesterB <sylvester.broich@gmail.com>
2023-07-17 16:22:01 +0200

    Change in telegram messaging system

---
+++ telegram_2.py
@@ -0,0 +1,22 @@
+import requests
+
+TOKEN = "6394746388:AAH9cXDsJb3caDc6_3wzuFLFMgnu7t46aH0"
+url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+print(requests.get(url).json())
+error_message = 'something'
+import requests
+TOKEN = "6394746388:AAH9cXDsJb3caDc6_3wzuFLFMgnu7t46aH0"
+chat_id = "5946134823"
+message = "error detected"
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+# print(requests.get(url).json()) # this sends the message
+
+def telebot(err_msg, out_msg):
+    url2 = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={err_msg, out_msg}"
+    print(requests.get(url2).json()) 
+
+
+
+
+# chat ID: 5946134823
+# bot ID: 6394746388:AAH9cXDsJb3caDc6_3wzuFLFMgnu7t46aH0
